043644ccf9dd5b6a3dc18cdfcabb3961383e20a4
Karina Vargas Gonzalez <kary_1806@hotmail.com>
2021-03-13 22:56:19 -0500

    First commit

---
+++ calculadora_go.go
@@ -0,0 +1,56 @@
+package calculadora_go
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type calc struct {
+}
+
+func (calc) operate(operacion string, operador string) int {
+
+	valores := strings.Split(operacion, operador)
+	fmt.Println(valores)
+	fmt.Println(valores[0] + valores[1])
+	numero1 := parsear(valores[0])
+	numero2 := parsear(valores[1])
+	total := 0
+	switch operador {
+	case "+":
+		total = numero1 + numero2
+		fmt.Println(numero1 + numero2)
+	case "-":
+		total = numero1 - numero2
+		fmt.Println(numero1 - numero2)
+	case "*":
+		total = numero1 * numero2
+		fmt.Println(numero1 * numero2)
+	default:
+		fmt.Println("no existe el operador")
+	}
+
+	return total
+
+}
+
+func parsear(entrada string) int {
+	numero, err := strconv.Atoi(entrada)
+	if err != nil {
+		fmt.Println("Error: format: int '<operator>' int")
+		os.Exit(1)
+	}
+	return numero
+}
+
+func leerEntrada() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Digite una operacion")
+	scanner.Scan()
+	operacion := scanner.Text()
+	fmt.Println(">>>>>>>>>", operacion)
+	return operacion
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kary1806/calculadora_go
+
+go 1.13
